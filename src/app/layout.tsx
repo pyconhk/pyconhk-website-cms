531bc7633434af881a416664d19425d00dce6f50
Alex Au <alexuau922@gmail.com>
2025-08-27 17:08:36 +0800

    Init commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,13 @@
+import '../styles/index.css'
+
+export default function RootLayout({
+  children
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html suppressHydrationWarning>
+      <body id="outstatic">{children}</body>
+    </html>
+  )
+}
